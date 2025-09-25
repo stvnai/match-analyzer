8f18d6911eca31095693bb5f8a870035eff03926
Steven Molina <steven.ms17@gmail.com>
2025-09-25 13:02:12 -0600

    Revert "Fixed avg match/torque calculation."
    This reverts commit 64499985f91256ab98ea2f26a0303deaca8b569d.
    
    Reverting changes

--- dash_app/matchanalyzer.py
+++ dash_app/matchanalyzer.py
@@ -195,8 +195,7 @@ def match_marker(data ,match_power= 200, match_length= 15, rest= 10, tolerance= 
     tolerance= tolerance/100
     min_required_values = int(tolerance*match_length)
 
-    df["matches"] = 0
-    df["marker"] = "o"
+    df["matches"] = pd.Series(0.0, dtype="Float64", index=df.index)
 
     df["match_count"] = 0
     in_match = False
@@ -216,7 +215,7 @@ def match_marker(data ,match_power= 200, match_length= 15, rest= 10, tolerance= 
         max_below_consecutive = 0
         
         for val in window:
-            if pd.isna(val) or val < match_power:
+            if val < match_power:
                 below_count +=1
                 max_below_consecutive = max(max_below_consecutive, below_count)
 
@@ -226,7 +225,7 @@ def match_marker(data ,match_power= 200, match_length= 15, rest= 10, tolerance= 
         if above_count >= min_required_values and max_below_consecutive <= rest:
 
             df.loc[i:i+match_length-1, "matches"] = df["power"].iloc[i:i+match_length]
-            df.loc[i:i+match_length-1, "marker"] = "x"
+
             if not in_match:
                 df.loc[i,"match_count"] = 1
                 in_match = True
@@ -245,16 +244,13 @@ def compute_avg_matches(df: pd.DataFrame):
     current_block = []
 
 
-    for i, row in df.iterrows():
-        if row["marker"] == "x":
+    for value in df["matches"]:
+        if pd.notna(value):
 
-            if pd.notna(row["matches"]):
-                current_block.append(row["matches"])
-        elif row["marker"] == "o":
-            if current_block:
+            current_block.append(value)
+        elif current_block:
                 avg_matches.append(sum(current_block) / len(current_block))
                 current_block = []
-                
 
     if current_block:
         avg_matches.append(sum(current_block) / len(current_block))
